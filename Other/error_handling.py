bc063443b8fed7da29011310f8ce305b829b9e31
yogurt1989 <mauropappaterra@gmail.com>
2019-12-07 14:59:13 +0100

    day 3 - part 1 - with poor performance

--- Other/error_handling.py
+++ Other/error_handling.py
@@ -3,3 +3,9 @@
 # Created by Mauro J. Pappaterra on 10 of October 2017.
 
 ############################################ ERROR HANDLING
+my_array = [1,2,3]
+
+try:
+  print(my_array[99])
+except:
+  print("ERROR!")
